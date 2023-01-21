30de672bfdf835eb7d49e29ca168fde2c99acf4b
Jeetu Gupta <jeetu270602@gmail.com>
2023-01-21 10:06:24 +0530

    add comments and push comment id's into its post

---
+++ controllers/comment_controller.js
@@ -0,0 +1,31 @@
+const comment = require('../models/commment');
+const post = require('../models/post');
+
+module.exports.create = function(req, res){
+
+    post.findById(req.body.post, function(err, post){
+        if(err){
+            console.log('Error in fetching post', err);
+            return ;
+        }
+        if(!post) return res.redirect('back');
+
+        comment.create({
+            content : req.body.content,
+            user : req.user.id,
+            post : req.body.post
+        }, function(err, newComment){
+            if(err){
+                console.log('Error in creating comment', err);
+                return ;
+            }
+            //pushing the comment id into its post
+            post.comments.push(newComment); //mongoose feature
+            //everytime we update we have to save it, save tells DB that it is the final version save it.
+            post.save();
+            return res.redirect('back');
+        })
+
+    });
+    
+}
